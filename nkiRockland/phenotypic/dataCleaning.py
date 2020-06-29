27d7c7a9247f15ff93462e1690012831ab309251
agt24 <adam.turnbull@yale.edu>
2020-06-29 17:29:27 -0400

    started writing code

---
+++ nkiRockland/phenotypic/dataCleaning.py
@@ -0,0 +1,22 @@
+# functions to clean the phenotypic data before merging
+
+def delEmptyRows(path, variables):
+    
+    # delete rows that contain no information
+    # path variable defines location of phenotypic data files, COINs download usually called 'assessment_data'
+    # variables defines the specific files you want to clean
+    
+    import os
+    import pandas as pd
+    from glob import glob
+    
+    os.chdir(path) # change path to location of data
+    files = sorted(glob('*')) # get list of data files
+    
+    for v in variables: # loop through each desired file
+        print("cleaning %s file" % v)
+        file = [s for s in files if v in s]
+        if len(file) > 1:
+            print("Warning, multiple files for variable %s" % v) # check that there is only one file for this variable
+        file = file[0]
+        if df.iloc[0][0] == 'ID': # check if first line contains repeat headers
